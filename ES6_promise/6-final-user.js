adab1af035b41e143a58a26e7ac38a738a07d81a
matthew25072005 <8855@holbertonstudents.com>
2024-07-25 06:45:03 -0300

    task 06

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
