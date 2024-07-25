4bf582e71cb567926fd0c4025c83bf21892dcee7
matthew25072005 <8855@holbertonstudents.com>
2024-07-25 04:29:41 -0300

    task 03

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,9 @@
-export function getStudentIdsSum(students) {
-    return students.reduce((sum, student) => sum + student.id, 0);
-}
+import { getListStudents } from './0-get_list_students.js';
+import { getStudentIdsSum } from './3-get_ids_sum.js';
+
+test("getStudentIdsSum is implemented correctly", () => {
+  const students = getListStudents();
+  const value = getStudentIdsSum(students);
+
+  expect(value).toEqual(8);
+});
