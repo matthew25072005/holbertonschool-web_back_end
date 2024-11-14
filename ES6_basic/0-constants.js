6e43a64e3308367fd51d9a2b80537a6fd29860bb
matthew25072005 <8855@holbertonstudents.com>
2024-11-14 16:25:55 -0300

    task 00

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,10 +1,6 @@
-// 0-constants.js
-
 export function taskFirst() {
-  part1 = 'I prefer const';
-  part2 = ' when I can.';
-  const task = `${part1}${part2}`;
-  return task
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
@@ -14,5 +10,6 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
