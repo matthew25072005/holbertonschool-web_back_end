89c9f723729317e7038f79495162a91f879e4fbd
matthew25072005 <8855@holbertonstudents.com>
2024-11-14 20:18:15 -0300

    task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const resarray = [...array];
+    let idx = 0;
+  
+    for (const value of array) {
+      resarray[idx] = appendString + value;
+      idx += 1;
+    }
+  
+    return resarray;
+  }
