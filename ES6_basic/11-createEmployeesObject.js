3c3ac93a3c1f9e1797cce7cfbe23222a00637c9a
matthew25072005 <8855@holbertonstudents.com>
2024-11-14 20:20:23 -0300

    task 11

---
+++ ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,6 @@
+export default function createEmployeesObject(departmentName, employees) {
+    const department = {
+      [departmentName.toLowerCase()]: employees,
+    };
+    return department;
+  }
