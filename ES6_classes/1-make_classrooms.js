6a58f7b1296b4260bf8cf7914aa49505d59efea9
matthew25072005 <8855@holbertonstudents.com>
2024-11-17 19:49:07 -0300

    task 01

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import Classroom from './0-classroom';
+
+function initializeRooms() {
+  const rooms = [new Classroom(19), new Classroom(20), new Classroom(34)];
+  return rooms;
+}
+
+export default initializeRooms;
