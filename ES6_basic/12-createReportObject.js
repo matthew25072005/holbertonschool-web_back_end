82cd6079928c1f4a95604016e6d23ba18222cdf3
matthew25072005 <8855@holbertonstudents.com>
2024-11-17 19:40:08 -0300

    task 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-    const list = {
-      allEmployees: {
-        ...employeesList,
-      },
-      getNumberOfDepartments: () => Object.keys(employeesList).length,
-    };
-    return list;
-  }
+  const list = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: () => Object.keys(employeesList).length,
+  };
+  return list;
+}
